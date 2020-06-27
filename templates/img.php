10aeb2b92735d27c07f343fd595ccdb79c968187
dennis-a-k <kargopolov.d.a@gmail.com>
2020-06-27 11:16:07 +0300

    lesson-4 php

--- templates/img.php
+++ templates/img.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../public/styles/style.css">
   <title>IMG</title>
 </head>
 <body>
-    <img src="img/<?= $_GET['photo']?>">
+  <img src="../public/img/<?= $_GET['photo']?>">
 </body>
 </html>
--- templates/index.php
+++ templates/index.php
@@ -4,15 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../public/styles/style.css">
   <title>Pictures</title>
 </head>
 <body class="grid">
 	<?php
-		$files = scandir('img');
+		$files = scandir('../public/img');
 		for ($i=2; $i < count($files); $i++):		
 	?>
-	<a href="img.php?photo=<?= $files[$i]?>" target="_blank"><img src="img/<?= $files[$i]?>"></a>
+	<a href="img.php?photo=<?= $files[$i]?>" target="_blank"><img src="../public/img/<?= $files[$i]?>"></a>
 	<?php
 		endfor;
 	?>
