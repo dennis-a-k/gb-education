dcb9a5b6e4544a5ca592f8d4d333d839d3cce5f0
dennis-a-k <kargopolov.d.a@gmail.com>
2020-06-27 11:01:42 +0300

    lesson-4 php

--- index.php
+++ index.php
@@ -1,92 +1,20 @@
-<?php
-    echo 'Задание №1<br>';
-    $a=0;
-    while($a < 100){
-        $a++;
-        if($a%3==0){
-            echo "$a ";
-        } 
-    }
-    echo '<br><hr><br>';
-
-    echo 'Задание №2<br>';
-    $i = 0;
-    do{
-        if($i == 0){
-            echo "$i - ноль<br>";
-        }elseif ($i%2 == 0) {
-            echo "$i - четное число<br>";
-        }else {
-            echo "$i - нечетное число<br>";
-        }
-        $i++;
-    }
-    while ($i <= 10);
-    echo '<hr><br>';
-
-    echo 'Задание №3<br>';
-    $cityes = [
-        'Московская обл.' => ['Зеленоград', 'Клин', 'Подольск'],
-        'Ленинградская обл.' => ['Всеволожск', 'Павловск', 'Кронштадт', 'Выборг'],
-        'Ростовкая обл.' => ['Ростов', 'Шахты']
-    ];
-    foreach ($cityes as $key => $value) {
-        if(is_array($value)){
-            $city = implode(', ',$value).';';
-        }
-        echo $key.' состоит из таких городов как: '.$city.'<br>';
-    }
-    echo '<hr><br>';
-
-    echo 'Задание №4<br>';
-    $alphabet = [
-        'а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'yo','ж'=>'zh','з'=>'z','и'=>'i','й'=>'y','к'=>'k',
-        'л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'ts','ч'=>'ch',
-        'ш'=>'sh','щ'=>'shch','ы'=>'y','э'=>'e','ю'=>'yu','я'=>'ya'
-    ];
-    echo strtr('русский алфавит', $alphabet);
-    echo '</br><hr></br>';
-
-    echo 'Задание №5<br>';
-    function spaces($str){
-        $symbol = [' ' => ' - '];
-        $text = strtr($str, $symbol);
-        echo "$text";
-    }
-    spaces('заменяем пробелы на дефис');
-    echo '</br><hr></br>';
-
-    echo 'Задание №6<br>';
-    $menu = [
-        'Города' => [
-            'Московская обл.' => ['Зеленоград', 'Клин', 'Подольск'],
-            'Ленинградская обл.' => ['Всеволожск', 'Павловск', 'Кронштадт', 'Выборг'],
-            'Ростовкая обл.' => ['Ростов', 'Шахты']
-        ]
-    ];
-    
-    function menuRender($arr){
-        $renderArr[] = '<ul>';
-        foreach ($arr as $key => $value) {
-            if (is_array($value)) {
-                $renderArr[] = '<li>'.$key.menuRender($value).'</li>';
-            } else {
-                $renderArr[] = '<li>'.$value.'</li>';
-            }
-        }
-        $renderArr[] = '</ul>';
-        return implode($renderArr);
-    }
-    echo menuRender($menu);
-    echo '<hr><br>';
-
-    echo 'Задание №7*<br>';
-    for ($i = 0; $i < 10; print $i++ . ' ') {};
-    echo '<br><hr><br>';
-
-    echo 'Задание №8*<br>';
-    echo '<br><hr><br>';
-
-    echo 'Задание №9*<br>';
-    spaces(strtr('замена русских букв английскими и пробелы дефисом', $alphabet));
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Pictures</title>
+</head>
+<body class="grid">
+	<?php
+		$files = scandir('img');
+		for ($i=2; $i < count($files); $i++):		
+	?>
+	<a href="img.php?photo=<?= $files[$i]?>" target="_blank"><img src="img/<?= $files[$i]?>"></a>
+	<?php
+		endfor;
+	?>
+</body>
+</html>
