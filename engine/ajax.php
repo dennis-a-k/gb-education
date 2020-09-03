9755d5a3ce31673e93ccbc45588cbd5748574114
dennis-a-k <kargopolov.d.a@gmail.com>
2020-09-03 16:23:57 +0300

    lesson 4 PHP2

--- engine/ajax.php
+++ engine/ajax.php
@@ -9,6 +9,7 @@ if (isset($_POST['start']) && is_numeric($_POST['start'])){
     $start = $_POST['start']; // точка старта выборки
  
     // получаем 5 записей начиная с точки старта
+    
     $res = $db->query("SELECT * FROM `goods` ORDER BY `id` DESC LIMIT {$start}, 5");
     while ($row = $res->fetchObject()){
         $articles[] = $row;
