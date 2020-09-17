9f8c547e6e0af910dc51335010fc422d8a59c6da
dennis-a-k <kargopolov.d.a@gmail.com>
2020-09-17 10:58:09 +0300

    lesson 7 PHP2

--- v/u_login.php
+++
@@ -1,5 +0,0 @@
-<form method="post">
-	<input type="text" name="login" placeholder="Введите логин" required>
-	<input type="password" name="password" placeholder="Введите пароль" required>
-	<input type="submit" name="button" value="Войти">
-</form>
