4678d74f50bc45ccac2c95897ef93edb087ec0ad
dennis-a-k <kargopolov.d.a@gmail.com>
2020-11-23 17:14:36 +0300

    lesson-3_JS-2

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@ class ProductsList{
     constructor(container = '.products'){
         this.container = container;
         this.goods = [];
-        this._fetchProducts();
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render();
+            });
     }
 
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Shirt', price: 150, img: 'img/Shirt.png'},
-            {id: 2, title: 'Socks', price: 50, img: 'img/Socks.png'},
-            {id: 3, title: 'Jacket', price: 350, img: 'img/Jacket.png'},
-            {id: 4, title: 'Shoes', price: 250, img: 'img/Shoes.png'},
-            {id: 5, title: 'Pants', price: 100, img: 'img/Pants.png'}
-        ];
+    _getProducts(){
+        return fetch(`/json/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     render(){
@@ -21,14 +23,6 @@ class ProductsList{
             const productObject = new ProductItem(product);
             block.insertAdjacentHTML('beforeend',productObject.render());
         });
-    }
-
-    //Метод, определяющий суммарную стоимость всех товаров
-    totalPrice(){
-        const totalPrice = document.querySelector('#total-price'); 
-        let sum = 0;
-        this.goods.forEach(product => sum += product.price);
-        totalPrice.innerText = `Итого: ${sum} ₽`;
     }
 }
 
@@ -51,29 +45,67 @@ class ProductItem{
 }
 
 let list = new ProductsList();
-list.render();
-list.totalPrice();
 
 //Корзина---------------------------------------------------------------------------------------------------------
 
 class Cart{
     constructor(){
         this.goods = [];
+        this.info = {};
+        this._getCartProducts()
+            .then(data => {
+                this.goods = [...data.contents];
+                this.info = {...data};
+                this.render();
+            });
     }
-    
+
+    _getCartProducts(){
+        return fetch(`/json/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     cost(){}
 
-    render(){}
+    render(){
+        document.querySelector('.cart-block').insertAdjacentHTML('beforeend',`<p>Сумма покупок: <span>${this.info.amount} ₽</span></p>`);
+        document.querySelector('.cart-block').insertAdjacentHTML('beforeend',`<p>Количество товара: <span>${this.info.countGoods} шт.</span></p><hr>`);
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+            document.querySelector('.cart-block').classList.toggle('invisible');
+        });
+
+        const block = document.querySelector('.cart-block');
+        this.goods.forEach(product => {
+            const productObject = new CartItem(product);
+            block.insertAdjacentHTML('beforeend',productObject.render());
+        });
+    }
 }
 
 class CartItem{
-    constructor(){}
+    constructor(product){
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.quantity = product.quantity;
+    }
 
     _plusCount(){}
 
     _minusCount(){}
 
-    _dellCount(){}
-
-    render(){}
+    render(){
+        return `<div>
+                    <button class="minus-plus" data-id="${this.id}">&#43;</button>
+                    <p>${this.title}</p>
+                    <span>${this.quantity} шт.</span>
+                    <p>${this.quantity * this.price} ₽</p>
+                    <button class="minus-plus" data-id="${this.id}">&#8722;</button>
+                </div>`
+    }
 }
+
+let cart = new Cart();
