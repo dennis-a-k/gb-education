3a483a3fd69c0957802bad1136f71a94b62a5701
dennis-a-k <kargopolov.d.a@gmail.com>
2020-12-02 23:09:11 +0300

    lesson-6_JS-2

--- js/SearchComponent.js
+++ js/SearchComponent.js
@@ -1,6 +1,10 @@
-Vue.component('search-form', {
-  props: ['searchLine'],
-  template: `<form action="#" class="search-form" @submit.prevent="$parent.$emit('filter-goods')">
+Vue.component('search', {
+  data(){
+    return{
+      searchLine: ''
+    }
+  },
+  template: `<form action="#" class="search-form" @submit.prevent="$parent.filterGoods(searchLine)">
               <input type="text" class="search-field" v-model="searchLine">
               <button class="btn-search" type="submit">
                 <img src="/img/search.png" alt="search">
